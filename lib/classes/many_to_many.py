089a366e22ecf1b456f06d740a02b877d3329970
Will Olson <will.olson.b@gmail.com>
2024-11-25 16:45:42 -0600

    Passing tests

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,34 +1,116 @@
+import math
 class Coffee:
     def __init__(self, name):
-        self.name = name
-        
+        if not isinstance(name, str):
+            raise ValueError("coffee name must be a str")
+        if len(name) < 2:
+            raise ValueError("coffee name must be >= 3 chars")
+        self._name = name
+
+    @property
+    def name(self):
+        return self._name
+
     def orders(self):
-        pass
+        return [order for order in Order.all if order.coffee == self]
     
     def customers(self):
-        pass
+        return list(set([order.customer for order in Order.all if order.coffee == self]))
     
     def num_orders(self):
-        pass
+        count = 0
+        for order in Order.all:
+            if order.coffee == self:
+                count += 1
+        return count
     
     def average_price(self):
-        pass
-
+        total = 0
+        for order in Order.all:
+            total += order.price
+        result = total / len(Order.all)
+        # return round(result)
+        return (math.floor(result * 10) / 10)
+    
 class Customer:
+    all = []
     def __init__(self, name):
-        self.name = name
+        if not isinstance(name, str):
+            raise ValueError("customer name must be a str")
+        if len(name) < 1 or len(name) > 15:
+            raise ValueError("customer name must be 1-15 chars")
+        self._name = name
+        Customer.all_customers(self)
+        
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, new_name):
+        if not isinstance(new_name, str):
+            raise ValueError("customer name must be a str")
+        self._name = new_name
+
+    @classmethod
+    def all_customers(cls, new_customer):
+        cls.all.append(new_customer)
         
     def orders(self):
-        pass
+        return [order for order in Order.all if order.customer == self]
     
     def coffees(self):
-        pass
+        return list(set([order.coffee for order in Order.all if order.customer == self]))
     
     def create_order(self, coffee, price):
-        pass
+        return Order(self, coffee, price)
+    
+    @classmethod
+    def most_aficionado(cls, coffee):
+        highest = float(0.0)
+        high_Customer = ""
+        for order in Order.all:
+            if order.coffee == coffee:
+                if order.price > highest:
+                    highest = order.price
+                    high_Customer = order.customer
+        if highest == 0:
+            return None
+        else:
+            return high_Customer
     
 class Order:
+    all = []
+
     def __init__(self, customer, coffee, price):
-        self.customer = customer
-        self.coffee = coffee
-        self.price = price
+        if not isinstance(customer, Customer):
+            raise ValueError("customer must be from class")
+        self._customer = customer
+        if not isinstance(coffee, Coffee):
+            raise ValueError("coffee must be from class")
+        self._coffee = coffee
+        if not isinstance(price, float):
+            if isinstance(price, int):
+                price = float(price)
+            else:
+                raise ValueError("price must be a float")
+        if price <= 1.0 or price >= 10.0:
+            raise ValueError("price must be a float")
+        self._price = price
+        Order.all_orders(self)
+
+    @property
+    def customer(self):
+        return self._customer
+    
+    @property
+    def coffee(self):
+        return self._coffee
+    
+    @property
+    def price(self):
+        return self._price
+
+    @classmethod
+    def all_orders(cls, new_order):
+        cls.all.append(new_order)
